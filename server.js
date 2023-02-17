9e3189553c0dcf51536b75be2651f0b91b110e4b
elleserdonaldson <ellessedon@gmail.com>
2023-02-17 22:20:53 +0000

    created back server with middleware

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-//bringing in express and
 const express = require("express");
 const app = express();
-//brining in the routes via modularization
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const { join } = require("path");
+
+const router = require("./routes");
 
 //setting up the port
 const PORT = process.env.PORT || 3001;
+
 //use is a method to configure the middleware used by the routes of the Express HTTP server object
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/html", htmlRoutes);
+app.use(express.static(join(__dirname, "public")));
+app.use(router);
 
-//used to bind and listen the connections on the specified host and port
+// running the server
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
