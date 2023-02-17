902a4f40ac1f1eac34a3e3a20bc58afee04b21d3
elleserdonaldson <ellessedon@gmail.com>
2023-02-17 22:07:16 +0000

    made a change to the structure for html routes

--- routes/webpageRoutes.js
+++ routes/webpageRoutes.js
@@ -1,14 +1,13 @@
 const router = require("express").Router();
-import { join } from "path";
 
 //renders the notes page
-router.get("/notes", (_req, res) => {
-  res.sendFile(join(__dirname, "../public/notes.html"));
+router.get("/notes", (req, res) => {
+  res.sendFile("./notes.html");
 });
 
 // this gets the route for the index.hmtl
-router.get("/", (_req, res) => {
-  res.sendFile(join(__dirname, "/public/index.html"));
+router.get("/", (req, res) => {
+  res.sendFile("./index.html");
 });
 
-export default router;
+module.exports = router;
