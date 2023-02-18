e6a0519654a74f0a2b4ca200a347ea02702686e2
elleserdonaldson <ellessedon@gmail.com>
2023-02-18 22:14:43 +0000

    incl path, tested the notes page with a consolelog

--- routes/webpageRoutes.js
+++ routes/webpageRoutes.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
+const path = require("path");
 
 //renders the notes page
 router.get("/notes", (req, res) => {
-  res.sendFile("./notes.html");
+  console.log("sending note page");
+  //res.sendFile("./notes.html");
+  const filePath = path.join(__dirname, "../public/notes.html");
+  res.sendFile(filePath);
 });
 
 // this gets the route for the index.hmtl
