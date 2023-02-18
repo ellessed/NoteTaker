853e7842e93bbd0382b23e2cc062b7c4aae5e8e5
elleserdonaldson <ellessedon@gmail.com>
2023-02-18 22:15:42 +0000

    removed comments, made chages to get and createapi

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,32 @@
 const { readFileSync } = require("fs");
-
+const { getDataFromFile, writeDataToFile } = require("../utils");
+const { v4: uuidv4 } = require("uuid");
 const router = require("express").Router();
 
 //read the notes
 router.get("/notes", (req, res) => {
   // get notes from the database(db.json)
-  const data = readFileSync("./db/db.json", "utf-8");
-
-  res.json(data);
+  // const data = readFileSync("./db/db.json", "utf-8");
+  const data = getDataFromFile();
+  console.log(data);
+  return res.json(data);
 });
 
-// you can write and read files in here without using a class
+router.post("/notes", (req, res) => {
+  console.log("saving note");
+  console.log(req.body);
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
 
+  const notes = getDataFromFile();
+  notes.push(newNote);
+
+  writeDataToFile(JSON.stringify(notes));
+  return res.json(newNote);
+});
 // readFileSync
 // writeFileSync
 
