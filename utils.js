7ed27a5c3cab7880c3230bfcd7416a999a60550f
elleserdonaldson <ellessedon@gmail.com>
2023-02-19 12:59:21 +0000

    comments added

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
+// read all data from file
 const getDataFromFile = () => {
   const jsonData = fs.readFileSync(
     path.join(__dirname, "./db/db.json"),
@@ -11,7 +11,7 @@ const getDataFromFile = () => {
 
   return data;
 };
-
+// write to it
 const writeDataToFile = (data) => {
   fs.writeFileSync(path.join(__dirname, "./db/db.json"), data);
 };
